9827eb1bbae88b30e9c9b3c10b24a07b869f75b0
Ray <89797110+Whatup364@users.noreply.github.com>
2022-02-03 11:29:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = ['917978303589'] // Put your number here
-global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.mods = [919861494774] // Want some help?
+global.prems = [919861494774] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
